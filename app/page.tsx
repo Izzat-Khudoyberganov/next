bebb1ef9fc854acd1ed6fedaaa654aef5da9cf6c
username <email@gmail.com>
2024-03-20 16:57:14 +0500

    added metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { Metadata } from 'next';
 
-
+export const metadata: Metadata = {
+  title: 'Avion',
+  description: 'Luxury homeware for people who love timeless design quality',
+  openGraph: {
+    images: ['/brand-img.png'],
+  },
+  keywords: ['sofa', 'avion', 'mebel', 'stoll', 'kursi', 'ustar', 'stull'],
+};
 
 export default function Home() {
   return <div className="flex gap-2">Home</div>;
